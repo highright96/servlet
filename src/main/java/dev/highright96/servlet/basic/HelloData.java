9e71d79e353446b176139e1aa418f794fbc38a35
Sangwoo96 <nsw5296@naver.com>
2021-04-10 22:19:54 +0900

    [Add] 프로젝트 생성, servlet, servletmvc

---
+++ src/main/java/dev/highright96/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package dev.highright96.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
